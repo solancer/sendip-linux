7e59664acca920502150e67c0eb9cbfa69cd0046
Srinivas Gowda <srinivas@solancer.com>
2014-04-17 04:11:41 +0530

    initi

--- sendip.sh
+++ sendip.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-unset HISTFILE;IPADDRESS=$(/sbin/ifconfig eth0 | sed -n 's/.*inet addr:\([^ ]*\).*/\1/p');echo -e 'The current IP address is: '$IPADDRESS'| mailx -s 'Ip address for Linux (Machine rebooted)' srinivas@solancer.com
+unset HISTFILE;IPADDRESS=$(/sbin/ifconfig eth0 | sed -n 's/.*inet addr:\([^ ]*\).*/\1/p');echo -e 'The current IP address for your system is: '$IPADDRESS''| mailx -s 'Ip address' srinivas@solancer.com
